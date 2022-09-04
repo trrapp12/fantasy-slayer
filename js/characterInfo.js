99b5dbfd516677252a0a322486b491a495491bac
Trevor Rapp <trapp@nuskin.com>
2022-09-04 00:00:01 -0600

    building out more character traits

---
+++ js/characterInfo.js
@@ -0,0 +1,92 @@
+
+const conscript = {
+  elId = 001;
+  characterName = ''; 
+  avatar = 'assets/assets/conscript.png'; 
+  race = 'human'; 
+  health = 100; 
+  weapon = 'Broad Sword'; 
+  skill = ''; 
+  speed = 30; 
+  strength = 75; 
+  distance = 67; 
+  backstory = 'put info here'; 
+  catchphrase = 'Choose not to be harmed—and you won\'t feel harmed. Don\'t feel harmed—and you haven\'t been.'; 
+  weakness = ['hunger','anger','greed']; 
+  relationship = 'extrovert'; 
+  intelligence = 89;
+}
+
+const ignisfatuus = {
+  elId = 002;
+  characterName = ''; 
+  avatar = 'assets/assets/ignisfatuus.png'; 
+  race = 'human'; 
+  health = 100; 
+  weapon = 'bow'; 
+  skill = ''; 
+  speed = 80; 
+  strength = 60; 
+  distance = 89; 
+  backstory = 'put info here'; 
+  catchphrase = 'Choose not to be harmed—and you won\'t feel harmed. Don\'t feel harmed—and you haven\'t been.'; 
+  weakness = ['rules','strength']; 
+  relationship = 'loyal'; 
+  intelligence = 100;
+}
+
+const soulforge = {
+  elId = 003;
+  characterName = ''; 
+  avatar = 'assets/assets/mage.png'; 
+  race = 'human'; 
+  health = 100; 
+  weapon = 'Broad Sword'; 
+  skill = ''; 
+  speed = 30; 
+  strength = 75; 
+  distance = 67; 
+  backstory = 'put info here'; 
+  catchphrase = 'Death may be the greatest of all human blessings'; 
+  weakness = ['hunger','anger','greed']; 
+  relationship = 'extrover'; 
+  intelligence = 89;
+}
+
+const naqualk = {
+  elId = 004;
+  characterName = ''; 
+  avatar = 'assets/assets/naqualk.png'; 
+  race = 'human'; 
+  health = 100; 
+  weapon = 'Broad Sword'; 
+  skill = ''; 
+  speed = 30; 
+  strength = 75; 
+  distance = 67; 
+  backstory = 'put info here'; 
+  catchphrase = 'Choose not to be harmed—and you won\'t feel harmed. Don\'t feel harmed—and you haven\'t been.'; 
+  weakness = ['hunger','anger','greed']; 
+  relationship = 'extrover'; 
+  intelligence = 89;
+}
+
+const mage = {
+  elId = 005;
+  characterName = ''; 
+  avatar = 'assets/assets/mage.png'; 
+  race = 'Though his memory reaches beyond life before the Shadow of the Great War brought upon the world the Trials of Dauus, and even the moment the first rays of light broke over the Great Mountains of Fire before they were tamed by the Ignis Fatuii, he cannot remember face of mother.  He was here before all other races. Only time, and magic, and nature are older than He.'; 
+  health = 50; 
+  weapon = 'time and space and magic'; 
+  skill = ''; 
+  speed = 10; 
+  strength = 75; 
+  distance = 10; 
+  backstory = 'put info here'; 
+  catchphrase = 'Choose not to be harmed—and you won\'t feel harmed. Don\'t feel harmed—and you haven\'t been.'; 
+  weakness = ['hunger','anger','greed']; 
+  relationship = 'extrover'; 
+  intelligence = 100;
+}
+
+
