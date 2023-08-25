31e41f7c6b9c856f67572441c600d709cf7f5f2e
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2023-08-25 01:22:32 -0600

    completes functionality for cycling through randomized characters and getting to the end of the game

--- js/index.js
+++ js/index.js
@@ -6,49 +6,6 @@ const player2Container = document.getElementById('character-2-art');
 const characterChoiceButton = document.getElementById('character-choice--button')
 const characterChoiceInput = document.getElementById('character-choice--input').value
 
-let myArray = ['conscript', 'ignisfatuus', 'mage', 'naqualk', 'soulforge']
-
-console.log(characterOrder(myArray))
-
-const characterOrder = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]
-    };
-    return array
-};
-
-
-// window.addEventListener('load', setCharacter)
-
-// function setCharacter() {
-//     switch(characterChoiceInput) {
-//         case 'conscript':
-//             currentCharacter = 'conscript';
-//             break;
-//         case 'ignisfatuus':
-//             currentCharacter = 'ignisfatuus';
-//             break;
-//         case 'mage':
-//             currentCharacter = 'mage';
-//             break;
-//         case 'naqualk':
-//             currentCharacter = 'naqualk';
-//             break;
-//         case 'conscript':
-//             currentCharacter = 'soulforge';
-//             break;
-//         case 'conscript':
-//             currentCharacter = 'soulforge';
-//             break;
-//         default:
-//             currentCharacter = 'conscript'
-//     }
-//     return currentCharacter
-// }
-
-// characterChoiceButton.addEventListener('click', setCharacter)
-
 function render() {
     player1Container.innerHTML = hero.renderCharacter();
     player2Container.innerHTML = villain.renderCharacter();
@@ -61,20 +18,41 @@ function attack() {
     villain.getDefendDiceHTML();
     hero.takeDamage(villain.currentDiceScore, hero.currentDefendDiceScore);
     villain.takeDamage(hero.currentDiceScore, villain.currentDefendDiceScore);
-    if (hero.dead || villain.dead) {
-        if (characterOrder.length > 0) {
-            setNextCharacter(characterOrder)
-        } else {
+    if (villain.dead) {
+        endGame()
+    } else if(hero.dead) {
+        if (shuffledArray.length > 0) {
+            hero = setNextCharacter();
+            render()
+        }
+        else {
             endGame()
         }
     }
     render()
 }
 
+let myArray = ['conscript', 'ignisfatuus', 'mage', 'naqualk', 'soulforge']
+
+const characterOrder = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]
+    };
+    console.log(array)
+    return array
+};
+
+let shuffledArray = characterOrder(myArray)
+
 function setNextCharacter() {
-    const nextCharacterData = characterData[characterOrder.shift()]
+    console.log(characterOrder)
+    const nextCharacterData = characterData[shuffledArray.shift()]
     return nextCharacterData ? new Character(nextCharacterData) : {}
 }
+
+
+console.log(typeof characterOrder(myArray))
 
 function endGame() {
     if (villain.health <=0 && hero.health <=0) {
@@ -89,7 +67,7 @@ function endGame() {
 document.getElementById('attack-button').addEventListener('click', attack)
 
 // create characters
-const hero = setNextCharacter()
+let hero = setNextCharacter()
 const villain = new Character(characterData.zedfire)
 console.log(villain)
 
