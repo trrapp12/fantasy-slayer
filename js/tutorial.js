6a996bf806a9fd2b46f2194c1dc7eaebe3793b6d
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2023-09-08 00:06:29 -0600

    corrects spelling error

--- js/tutorial.js
+++ js/tutorial.js
@@ -17,7 +17,7 @@ export function tutorial() {
                 <li>Clicking 'To Battle' will roll for both you and the enemy.</li>
                 <li>Attack dice are 6 sided.  Defense dice are 10 sided.</li>
                 <li>Attack points are calculated by summing up all dice.  For example: a roll of 1, 2, 3 would be 1 + 2 + 3 = 6.</li>
-                <li>If you roll matches, all matches will be multiplied together first. Then the remaining numbers will be added. For example, a roll of 1, 3, 3 would be (3 X 3) + 1 = 10. It's possible to get multiple sets of matches. For instance, a roll of 3, 3, 4, 4, 4 would be 3 X 3 X 4 X 4 X 4 = 576.</li>
+                <li>If your roll matches, all matches will be multiplied together first. Then the remaining numbers will be added. For example, a roll of 1, 3, 3 would be (3 X 3) + 1 = 10. It's possible to get multiple sets of matches. For instance, a roll of 3, 3, 4, 4, 4 would be 3 X 3 X 4 X 4 X 4 = 576.</li>
                 <li>Red dice are for defense.</li>
                 <li>The amount you roll for defense represents a percentage out of 100.  For example: a roll of 9 would mean you defended 90% of the attack.  So if your opponent's original attack was 100 points, you would only receive 10 points of damage.</li>
                 <li>Each character has 1 opportunity for a specialized attack, represented by the icons on the left. The icon will become highlighted when it can be used. Some attacks can only be used by certain characters. It is not required to use these, but conquering without them will be very challenging. A good roll can turn the tide of the game, but be cautious: a bad roll can be disastrous for you.</li>
