804367b296b6cb5e0422ed77e64ccbd0ff7b5f97
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2023-09-07 23:36:07 -0600

    adds CSS for tutorial and clarifies instructions

--- js/animation.js
+++ js/animation.js
@@ -1,17 +1,17 @@
-(() => {
+import { tutorial } from './tutorial.js'
+
   const title = document.getElementById('title')
   const introModal = document.getElementById('intro-modal');
   
   const introModalClose = document.getElementById('intro-modal-text-container');
 
-
   window.addEventListener('load', () => {
     setTimeout(() => {
       title.classList.add('backlight');
+      tutorial();
     }, 66005)
     setTimeout(() => {
       introModal.style.display = "none"
     }, 66000)
     
   })
-})();
---
+++ js/tutorial.js
@@ -0,0 +1,35 @@
+export function tutorial() {
+    console.log('tutorial.js fired')
+    const docBody = document.getElementsByTagName('body')[0];
+    console.log(docBody)
+    const container = document.createElement('div')
+    container.setAttribute('id', 'tutorial-modal-container')
+    console.log(container)
+    const content = `
+        <h1>Welcome to Fantasy Slayer</h1>
+        <br/>
+        <div class="tutorial-inner-border">
+        <p>This is a simple tutorial to acquaint you with game play</p>
+        <br/>
+            <ol>
+                <li>You are the player on the left</li>
+                <li>You will play through 5 separate characters attempting to kill the villain -- Zedfire</li>
+                <li>Clicking 'To Battle' will roll for both you and the enemy</li>
+                <li>Attack dice are 6 sided.  Defense dice are 10 sided</li>
+                <li>Attack points are calculated by summing up all dice.  For example: a roll of 1, 2, 3 would be 1 + 2 + 3 = 6.</li>
+                <li>If you roll matches, all matches will be multiplied together first -- then the remaining will be added.  For example: a roll of 1 , 3, 3 would be (3 X 3) + 1 = 10.  It is possible to get multiples sets of multiples.  For example: 3, 3, 4, 4, 4 would be 3 X 3 X 4 X 4 X 4 = 576</li>
+                <li>Red dice for defense.</li>
+                <li>The amount you roll for defense represents a percentage out of 100.  For example: a roll of 9 would mean you defended 90% of the attack.  So if your opponents original attack was 100 points, you would only receive 10 points of damage.</li>
+                <li>Each character has 1 opportunity for a specialized attack, represented by the icons on the left.  The icon will become highlighted when it can be used.  Some attacks can only be used by certain characters.  It is not required to use these, but it will be most difficult to conquer without them. A good roll can turn the tide of the game, but be careful, a bad roll can be disastrous for you.</li>
+            </ol>
+            <button id="quest-button" class="quest-button">Begin Your Quest</button>
+        </div>`
+    container.classList.add('tutorial-modal')
+    container.innerHTML = content
+    console.log('container', container)
+    docBody.insertAdjacentElement("afterbegin", container)
+    document.getElementById('quest-button').addEventListener('click', () => {
+        container.style.display = 'none'
+})
+};
+    
