58259e9db0bd23e0dcb291c75c9813f4ffe94bef
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2023-10-13 10:46:09 -0600

    adds console.logs

--- js/utils.js
+++ js/utils.js
@@ -46,16 +46,23 @@ function hasDuplicates (arr){
 }
 
 function calculateEnhancedScore (obj, arr){
+    console.log('object is: ', obj)
+    console.log('arr is: ', arr)
     // obj is the result of calling findDuplicateIndices(attackScoreArray)  
     // Returns an obj in the form { '1' : [2, 4]} where the 1 is the number repeated on the dice, 
     // and the values in the array are the indices where 1 would appear on the dice
     // arr is the original attackScoreArray, brought back in again to take into account numbers which weren't repeated
     let total = 0;
+    console.log('total is :', total)
     // need to keep this total in here.  Without doing a += within the loop, it can't account for two sets of pairs i.e. a dice roll of [2, 2, 3, 3, 3]
     let addends;
+    console.log('addends are: ', addends)
     let multiplicant;
+    console.log('multiplicants are: ' , multiplicant)
     for (const [key, value] of Object.entries(obj)) {
+        console.log('inside for loop, key is: ', key, 'value is: ', value)
         multiplicant = Number(key) ** value.length
+        console.log('multiplicant is: ', multiplicant)
         addends = arr.filter(arrValue => arrValue !== Number(key)).reduce((acc, curr) => { return acc += curr}, 0) 
         total += addends + multiplicant
     }
