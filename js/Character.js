f0989153b62c4367e009140c579f91b9f62885ec
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2023-11-10 20:22:10 -0700

    updated comments

--- js/Character.js
+++ js/Character.js
@@ -20,7 +20,16 @@ class Character {
         this.defendDiceValue = ''
         this.spells = spells
     }
-    
+
+    /* 
+
+    FUNCTION PURPOSE: getIndexesOfDiceScoreMatches gets the indices of the matches so we know which element to highlight visually
+    REQUIRED INPUT/DEPENDENCIES: an array
+    OUTPUT: array
+    SIDE EFFECTS: returns a sorted array of the correct indices that need to be highlighted
+
+    */ 
+
     getIndexesOfDiceScoreMatches (arr) {
         const indexOfDuplicates = [];
         const seenIndexes = {};
@@ -33,8 +42,18 @@ class Character {
             seenIndexes[arr[i]] = i;
           }
         }
+
         return Array.from(new Set(indexOfDuplicates)).sort((a,b) => a-b);
     }
+
+    /* 
+
+    FUNCTION PURPOSE: creates an array that represents the player's defensive dice roll and then renders HTML to the page for that roll
+    REQUIRED INPUT/DEPENDENCIES: N/A
+    OUTPUT: HTML
+    SIDE EFFECTS: visually displays the Defensive dice for both Player and Villain
+
+    */ 
 
     setDefendDiceHTML () {
         this.defendDiceScore = getDiceRollArray(1, 10)
@@ -52,6 +71,15 @@ class Character {
         })
 
     }
+
+    /* 
+
+    FUNCTION PURPOSE: controls order of calling 1) offensive dice roll 2) render Banner 3) get matches 4) rendering dice to page;
+    REQUIRED INPUT/DEPENDENCIES: N/A
+    OUTPUT: logic & HTML
+    SIDE EFFECTS: dice roll, banner display, highlighting matches, rendering offensive dice
+
+    */ 
 
     getDiceHTML () {
         console.log('getDiceHTML() firing')
@@ -76,6 +104,14 @@ class Character {
         this.diceArrayForRendering = this.diceArray.join('');
     }
 
+    /* 
+
+    FUNCTION PURPOSE: creates a string to be displayed of the matches on the offensive dice and puts a "X" symbol to indicate multiplied ... i.e. "3 x 3 x 3" for a roll with 3 3's
+    REQUIRED INPUT/DEPENDENCIES: and object
+    OUTPUT: string ... i.e. "2 x 2" of "3 x 3 x 3" or "5 x 5 x 4 x 4 x 4"
+    SIDE EFFECTS: creates string which will be returned
+
+    */ 
 
     renderMultiplesForFlyOutMessage (obj) {
         let messagesArr = [];  
@@ -91,15 +127,22 @@ class Character {
         this.duplicates = {}
     }
 
+    /* 
+
+    FUNCTION PURPOSE: controls steps to determine how much damage an attack is worth, how much it is buffered after defending calculated, 
+    and if there were matches how they are multiplied or if none how they are added, if they need to die, and then advances the turn
+    REQUIRED INPUT/DEPENDENCIES: attackScoreArray...what player is getting attacked with, and defendDiceScore...what they defended with
+    OUTPUT: string ... i.e. "2 x 2" of "3 x 3 x 3" or "5 x 5 x 4 x 4 x 4"
+    SIDE EFFECTS: updates state of health, death, and turns
+
+    */ 
+
     takeDamage (attackScoreArray, defendDiceScore) {
-        // attackScoreArray begins here as an arbitrarily named parameter.  
-        // It is what the current character is getting attacked by, 
-        // not what they are attacking the other person with.
+    /* attackScoreArray begins here as an arbitrarily named parameter. It is what the current character is getting attacked by, not what they are attacking the other person with. */
         const valueToIndices = {};
     
         const findDuplicateIndices = (arr) => {
-            // I decided not to make this a utility function because if you move it out of here, it can't find valueToIndices, and if I move valueToIndices
-            // inside the function then it can't find this.duplicates.
+            /* I decided not to make this a utility function because if you move it out of here, it can't find valueToIndices, and if I move valueToIndices inside the function then it can't find this.duplicates. */
             arr.forEach((value, index) => {
                 if (!valueToIndices[value]) {
                     valueToIndices[value] = [index];
@@ -116,16 +159,15 @@ class Character {
         };
         
         if (hasDuplicates(attackScoreArray)) {
-            // currentDiceScore should not be used to calculate the attack, only to check for duplicates.  attackScoreArray is the opposite character, not the this character
+            /* currentDiceScore should not be used to calculate the attack, only to check for duplicates.  attackScoreArray is the opposite character, not the this character */
             this.totalDamage = calculateEnhancedScore(findDuplicateIndices(attackScoreArray), attackScoreArray)
-            // here we have detected there are multiples, so we are iterating over them to find which ones, 
-            // what the values are, and then timesing them together instead of adding them
+            /* here we have detected there are multiples, so we are iterating over them to find which ones, what the values are, and then timesing them together instead of adding them */
             this.bufferedDamage = this.totalDamage - (this.totalDamage * (this.defendDiceScore[0] * .10));
-            // the defendDiceScore is the percentage of the number they roll...i.e roll a 9 means you defended 90% of the attack, a 10 -> 10%
+            /* the defendDiceScore is the percentage of the number they roll...i.e roll a 9 means you defended 90% of the attack, a 10 -> 10% */
             this.health = this.health - Math.floor(this.bufferedDamage);
         } else {
             this.totalDamage = attackScoreArray.reduce((accumulator, currentVal) => {return accumulator + currentVal}, 0);
-            // here .reduce can be used because if no dice are repeated we are simply adding them together
+            /* here .reduce can be used because if no dice are repeated we are simply adding them together */
             this.bufferedDamage = this.totalDamage - (this.totalDamage * (this.defendDiceScore[0] * .10));
             this.health = this.health - Math.floor(this.bufferedDamage);
 
@@ -174,6 +216,15 @@ class Character {
             weapon
         } = this;
 
+        /* *********************************************** BANNER LOGIC *********************************************** 
+
+        FUNCTION PURPOSE: heroDisplayLogic and renderBanner logic controls the display logic for the fly-in if a hero rolls matching dice
+        REQUIRED INPUT/DEPENDENCIES: renderBanner is set by hasDuplicates(), when hasDuplicates() is called within the getDiceHTML() method
+        OUTPUT: the display banner that flys in from the left
+        SIDE EFFECTS: displays the banner 
+
+        */ 
+
         let heroDisplayLogic;
 
         if (renderBanner === true && characterClass === 'hero') {
@@ -182,19 +233,34 @@ class Character {
             heroDisplayLogic =''
         }
 
-
         let villainDisplayLogic;
 
-            if (renderBanner === true && characterClass === 'villain') {
-                villainDisplayLogic =  `<div class="power-hit-villain-container"><p class="power-hit-villain">The Furies, the Fates, the Death-Daimones and Thanatos himself have conspired for your demise.  ${characterName}'s attack is increased to</p><div class="message">${(messages) ? messages : ''}</div></div>`
-            } else {
-                villainDisplayLogic = ''
-            }       
+        if (renderBanner === true && characterClass === 'villain') {
+            villainDisplayLogic =  `<div class="power-hit-villain-container"><p class="power-hit-villain">The Furies, the Fates, the Death-Daimones and Thanatos himself have conspired for your demise.  ${characterName}'s attack is increased to</p><div class="message">${(messages) ? messages : ''}</div></div>`
+        } else {
+            villainDisplayLogic = ''
+        }    
+        
+        /* *********************************************** GRAPH-CONTAINER LOGIC *********************************************** 
+
+        (WIP, TRYING TO GET IT TO DRAW DYNAMICALLY INSTEAD OF JUST RE-RENDER EVERY TIME)
+
+        FUNCTION PURPOSE: graphContainer holds the health bars.  renderHealthChart renders them
+        REQUIRED INPUT/DEPENDENCIES: original health from character-data.js and correct output from renderHealthChart
+        OUTPUT: the circular health charts
+        SIDE EFFECTS: displays the circular health charts
+
+        */ 
         
         const graphContainer = document.querySelectorAll('.character-stats--health-graph')
         console.log('graphContainers are ', graphContainer)
         let graph; 
         console.log('graph', graph)
+
+        /* 
+        length must be 3 here, because the first time nothing is there, it writes the first graph, turning it to 1, the second time it
+        renders the villain graph, turning the length to 2.  So it must write 2 times, but not the third
+        */ 
 
         if (graphContainer.length < 3) {
             console.log('inside graphContainer.length < 3', graph)
